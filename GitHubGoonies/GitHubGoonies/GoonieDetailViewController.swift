e4a1674eb24ebb3b1951a42c94b51efeac5ddb28
alexanderceoh <alexanderceoh@gmail.com>
2015-09-28 21:53:05 -0400

    Goonies Plus

---
+++ GitHubGoonies/GitHubGoonies/GoonieDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GoonieDetailViewController.swift
+//  GitHubGoonies
+//
+//  Created by Alex Oh on 9/28/15.
+//  Copyright © 2015 Alex Oh. All rights reserved.
+//
+
+import Foundation
+
+class GoonieDetailViewController: UITableViewController {
+    
+    func simpleDetail() {
+        
+        print(
+        
+    }
+    
+}
